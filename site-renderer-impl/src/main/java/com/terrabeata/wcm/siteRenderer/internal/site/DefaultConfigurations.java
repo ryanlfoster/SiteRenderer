ec3f414e6f17db4d858cd622c16fdfb8fc9d6072
Deke Smith <deke@terrabeata.com>
2013-12-27 23:23:30 -0600

    Create stub for configuration creating class

---
+++ site-renderer-impl/src/main/java/com/terrabeata/wcm/siteRenderer/internal/site/DefaultConfigurations.java
@@ -0,0 +1,22 @@
+package com.terrabeata.wcm.siteRenderer.internal.site;
+
+import javax.jcr.Node;
+import javax.jcr.Property;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+
+public class DefaultConfigurations {
+	private static final String PERSISTENT_DATA_PATH = "/var/siteRender/intallDate";
+	
+	public void createDefaultConfigurations(ResourceResolver resourceResolver) {
+		Resource installData = resourceResolver.getResource("/var/siteRender/i001");
+		if (installData != null) {
+			
+		}
+	}
+	
+	public void createDefaultQueue(ResourceResolver resourceResolver) {
+		
+	}
+}
